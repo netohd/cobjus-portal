a3dce5e299c2a87b0e0cf81698c0a6d20c4a99e4
Lucas Teodoro <lucasteodoroac@gmail.com>
2023-09-16 15:55:11 -0300

    login 1

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,17 +1,146 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-import { Grid, Stack } from '@mui/material'
-import BGLogin from "@/assets/images/BG.svg"
-export default function MainPage() {
+'use client'
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import { ThemeProvider } from '@mui/material/styles';
+import { useState } from 'react';
+
+function Copyright(props: any) {
   return (
-    <Grid container spacing={2}>
-      <Grid item xs={6}>
-        <Stack direction="row" justifyContent="center" alignItems="center">
-          <Image src={BGLogin} alt="Picture of the author" />
-        </Stack>
-      </Grid>
-      <Grid item xs={6}>
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://www.algar.com.br/" >
+        Grupo Algar
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+// TODO remove, this demo shouldn't need to reset the theme.
+
+export default function SignInSide() {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  const [cpf, setCpf] = useState('')
+  const maskCpfCnpj = async (e: any) => {
+    let target = e.target?.value
+    let newTarget = ""
+
+    if (target.length <= 14) {
+      newTarget = target
+        .replace(/\D/g, '')
+        .replace(/(\d{3})(\d)/, '$1.$2')
+        .replace(/(\d{3})(\d)/, '$1.$2')
+        .replace(/(\d{3})(\d{1,2})/, '$1-$2')
+        .replace(/(-\d{2})\d+?$/, '$1')
+    } else {
+      newTarget = target
+        .replace(/\D/g, '')
+        .replace(/(\d{2})(\d)/, '$1.$2')
+        .replace(/(\d{3})(\d)/, '$1.$2')
+        .replace(/(\d{3})(\d)/, '$1/$2')
+        .replace(/(\d{4})(\d)/, '$1-$2')
+        .replace(/(-\d{2})\d+?$/, '$1')
+
+    }
+    setCpf(newTarget)
+  }
+
+  return (
+    <Grid container component="main" sx={{ height: '100vh', textAlign: 'center', alignItems: '' }}>
+      <CssBaseline />
+      <Grid
+        item
+        xs={false}
+        sm={4}
+        md={7}
+        sx={{
+          backgroundImage: 'url(https://source.unsplash.com/random?wallpapers)',
+          backgroundRepeat: 'no-repeat',
+          backgroundColor: (t) =>
+            t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+        }}
+      />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <Box
+          sx={{
+            my: 8,
+            mx: 4,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Grid item style={{ alignItems: 'flex-start', marginBottom: '20%' }}>
+            <img src="https://i.ibb.co/28g6LWq/Algar.png" alt="Logo Algar" style={{ width: '35%', minWidth: '100px' }} />
+          </Grid>
+
+
+          <Typography component="h1" variant="h6">
+            Consulte suas negociações aqui
+          </Typography>
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }} >
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="cpfCnpj"
+              label="CPF / CNPJ"
+              type="cpfCnpj"
+              id="cpfCnpj"
+              value={cpf}
+              onChange={maskCpfCnpj}
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              sx={{ textAlign: 'left', justifyContent: 'left' }}
+              control={<Checkbox value="remember" color="primary" />}
+              label="Eu aceito os Termos e Políticas de Privacidade."
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Consultar
+            </Button>
+            {/* <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Esqueceu sua senha?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid> */}
+            <Copyright sx={{ mt: 5 }} />
+          </Box>
+        </Box>
       </Grid>
     </Grid>
-  )
+  );
 }
